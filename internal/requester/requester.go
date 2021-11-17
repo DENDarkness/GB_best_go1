560a81a45030ffa5d6c2dd325f2566c385633f55
Denis Bulavin <d.bulavin81@yandex.ru>
2021-11-17 23:03:58 +0300

    Переделал структуру приложения

---
+++ internal/requester/requester.go
@@ -0,0 +1,62 @@
+package requester
+
+import (
+	"context"
+	"lesson1/internal/loggerDebugWrap"
+	"lesson1/internal/page"
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type Requester interface {
+	Get(ctx context.Context, url string) (page.Page, error)
+}
+
+type requester struct {
+	timeout time.Duration
+	logger *zap.SugaredLogger
+	debugType bool
+}
+
+func NewRequester(timeout time.Duration, logger *zap.SugaredLogger, dt bool) requester {
+	return requester{
+		timeout: timeout,
+		logger: logger,
+		debugType: dt,
+	}
+}
+
+func (r requester) Get(ctx context.Context, url string) (page.Page, error) {
+	select {
+	case <-ctx.Done():
+		return nil, nil
+	default:
+		cl := &http.Client{
+			Timeout: r.timeout,
+		}
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			r.logger.Errorf("Failed to create NewRequest: %v", err)
+			return nil, err
+		}
+		body, err := cl.Do(req)
+		if err != nil {
+			r.logger.Errorf("Failed to complete the request: %v", err)
+			return nil, err
+		}
+		defer body.Body.Close()
+		page, err := page.NewPage(body.Body)
+		if err != nil {
+			r.logger.Errorf("Failed to create the NewPage: %v", err)
+			return nil, err
+		}
+		if r.debugType {
+			p := loggerDebugWrap.NewloggerPageWrap(page, r.logger)
+			return p, nil
+		}
+
+		return page, nil
+	}
+}
